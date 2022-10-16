c4672e3f2a69023350237f457304536493beca29
Julien Widmer <ju.widmer@pm.me>
2022-10-16 13:35:17 -0400

    Implement user creation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,10 +6,25 @@ Student ID:     101320111
 */
 const express = require("express");
 const routes = express.Router();
+const UserModel = require("../models/userModel");
 
 // http://localhost:8081/api/user/signup
-routes.post("/signup", (req, res) => {
-    res.send({message: "201 - Allow user to create new account"});
+routes.post("/signup", async (req, res) => {
+    // Validate request
+    if(JSON.stringify(req.body) == "{}") {
+        // Client side error
+        return res.status(400).send({message: "User content can not be empty"});
+    }
+
+    // Create new User
+    const newUser = new UserModel(req.body);
+    try {
+        await newUser.save();
+        res.status(201).send(newUser);
+    } catch (error) {
+        // Server side error
+        res.status(500).send({message: `Error while inserting new user: ${error}`});
+    }
 })
 
 // http://localhost:8081/api/user/login
