1919cec9240f5a251d88382559aec0535277312a
Julien Widmer <ju.widmer@pm.me>
2022-10-18 12:18:24 -0400

    Implement get employee

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -45,18 +45,32 @@ routes.post("/employees", async (req, res) => {
     }
 })
 
-// http://localhost:8081/api/emp/employees/1
-routes.get("/employees/:id", (req, res) => {
-    res.send({message: "200 - User can get employee details by employee id"})
+// http://localhost:8081/api/emp/employees/ObjectId
+routes.get("/employees/:id", async (req, res) => {
+    // Retrieve single Employee with ObjectId
+    try {
+        const employee = await EmployeeModel.findById(req.params.id);
+        if (employee) {
+            res.status(200).send(employee);
+        } else {
+            // Client side error
+            res.status(400).send({message: `No employee found with employeeId: ${req.params.id}`});
+        }
+    } catch (error) {
+        // Server side error
+        res.status(500).send(
+            {message: `Error while retrieving employee with given employeeId "${req.params.id}": ${error}`}
+        );
+    }
 })
 
-// http://localhost:8081/api/emp/employees/1
+// http://localhost:8081/api/emp/employees/ObjectId
 routes.put("/employees/:eid", (req, res) => {
     const employeeId = req.params.eid;
     res.send({message: "200 - User can update employee details"})
 })
 
-// http://localhost:8081/api/emp/employees?eid=1
+// http://localhost:8081/api/emp/employees?eid=ObjectId
 routes.delete("/employees", (req, res) => {
     const employeeId = req.query.eid;
     res.send({message: "204 - User can delete employee by employee id"})
