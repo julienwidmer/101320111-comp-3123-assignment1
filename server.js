50b7ed1f3179cdc4bede1405ad39672d9637c1ca
Julien Widmer <ju.widmer@pm.me>
2022-11-30 22:29:28 -0500

    Fix missing CORS header

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ const SERVER_PORT = 8081;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 // Routing config
