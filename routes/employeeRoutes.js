2791bf0e3ecd8f43c3756394e058e7d10b75b856
Julien Widmer <ju.widmer@pm.me>
2022-12-03 01:30:53 -0500

    Improve error validation

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -7,6 +7,47 @@ Student ID:     101320111
 const express = require("express");
 const routes = express.Router();
 const EmployeeModel = require("../models/employeeModel");
+const validator = require("validator");
+
+function validateEmployeeModel(requestBody) {
+    // Check for required values
+    if (!requestBody.hasOwnProperty("first_name") ||
+        requestBody.first_name.trim().length < 1 || requestBody.first_name.trim().length > 100) {
+        // Client side error -> field empty or longer than 100 characters
+        return "First name can't be empty or longer than 100 characters.";
+    }
+
+    if (!requestBody.hasOwnProperty("last_name") ||
+        requestBody.last_name.trim().length < 1 || requestBody.last_name.trim().length > 50) {
+        // Client side error -> field empty or longer than 50 characters
+        return "Last name can't be empty or longer than 50 characters.";
+    }
+
+    if (!requestBody.hasOwnProperty("email") ||
+        requestBody.email.trim().length < 1 || requestBody.email.trim().length > 50) {
+        // Client side error -> field empty or longer than 50 characters
+        return "Email address can't be empty or longer than 50 characters.";
+    }
+
+    if (!validator.isEmail(requestBody.email)) {
+        // Client side error -> invalid email address format
+        return "Email address must be valid.";
+    }
+
+    if (!requestBody.hasOwnProperty("gender") ||
+        !["Male", "Female", "Other"].includes(requestBody.gender)) {
+        // Client side error -> field empty or longer than 50 characters
+        return "Gender can't be empty or different than 'Male', 'Female' or 'Other'.";
+    }
+
+    if (!requestBody.hasOwnProperty("salary") ||
+        !(typeof requestBody.salary === "number") || requestBody.salary < 0) {
+        // Client side error -> field empty or longer than 50 characters
+        return "Salary can't be empty and should be a valid numerical value greater or equal to 0.";
+    }
+
+    return null
+}
 
 // http://localhost:8081/api/emp/employees
 routes.get("/employees", async (req, res) => {
@@ -31,8 +72,12 @@ routes.post("/employees", async (req, res) => {
     // Validate request
     if(JSON.stringify(req.body) == "{}") {
         // Client side error
-        return res.status(400).send({message: "Employee content can not be empty"});
+        return res.status(400).send({message: "Employee content can't be empty"});
     }
+
+    // Handle errors
+    const errorMessage = validateEmployeeModel(req.body);
+    if (errorMessage != null) { return res.status(400).send({message: errorMessage}); }
 
     // Create new Employee
     const newEmployee = new EmployeeModel(req.body);
@@ -40,8 +85,13 @@ routes.post("/employees", async (req, res) => {
         await newEmployee.save();
         res.status(201).send(newEmployee);
     } catch (error) {
-        // Server side error
-        res.status(500).send({message: `Error while inserting new employee: ${error}`});
+        if (error.toString().includes("duplicate key error")) {
+            // Client side error -> duplicate email address
+            return res.status(400).send({message: "Email address must be unique."});
+        } else {
+            // Server side error
+            return res.status(500).send({message: `Employee not created: ${error}`});
+        }
     }
 })
 
@@ -71,8 +121,12 @@ routes.put("/employees/:eid", async (req, res) => {
     // Validate request
     if(JSON.stringify(req.body) == "{}") {
         // Client side error
-        return res.status(400).send({message: "Employee content can not be empty"});
+        return res.status(400).send({message: "Employee content can't be empty"});
     }
+
+    // Handle errors
+    const errorMessage = validateEmployeeModel(req.body);
+    if (errorMessage != null) { return res.status(400).send({message: errorMessage}); }
 
     // Update Employee with employeeId
     try {
@@ -86,10 +140,13 @@ routes.put("/employees/:eid", async (req, res) => {
             res.status(400).send({message: `No employee to update found with employeeId: ${req.params.noteId}`});
         }
     } catch (error) {
-        res.status(500).send(
+        if (error.toString().includes("duplicate key error")) {
+            // Client side error -> duplicate email address
+            return res.status(400).send({message: "Email address must be unique."});
+        } else {
             // Server side error
-            {message: `Error while updating employee with given employeeId "${employeeId}": ${error}`}
-        );
+            return res.status(500).send({message: `Employee not updated: ${error}`});
+        }
     }
 })
 
