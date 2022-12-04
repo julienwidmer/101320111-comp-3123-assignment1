f9de7e3ecad63fb93313a38d9fc43ca7dbc85c42
Julien Widmer <ju.widmer@pm.me>
2022-12-04 04:09:19 -0500

    Improve error validation for User API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,29 @@ Student ID:     101320111
 const express = require("express");
 const routes = express.Router();
 const UserModel = require("../models/userModel");
+const validator = require("validator");
+
+function validateUserModel(requestBody) {
+    // Check for required values
+    if (!requestBody.hasOwnProperty("username") ||
+        requestBody.username.trim().length < 1 || requestBody.username.trim().length > 100) {
+        // Client side error -> field empty or longer than 100 characters
+        return "Username can't be empty or longer than 100 characters.";
+    }
+
+    if (!requestBody.hasOwnProperty("email") ||
+        requestBody.email.trim().length < 1 || requestBody.email.trim().length > 50) {
+        // Client side error -> field empty or longer than 50 characters
+        return "Email address can't be empty or longer than 50 characters.";
+    }
+
+    if (!validator.isEmail(requestBody.email)) {
+        // Client side error -> invalid email address format
+        return "Email address must be valid.";
+    }
+
+    return null
+}
 
 // http://localhost:8081/api/user/signup
 routes.post("/signup", async (req, res) => {
@@ -16,14 +39,23 @@ routes.post("/signup", async (req, res) => {
         return res.status(400).send({message: "User content can not be empty"});
     }
 
+    // Handle errors
+    const errorMessage = validateUserModel(req.body);
+    if (errorMessage != null) { return res.status(400).send({message: errorMessage}); }
+
     // Create new User
     const newUser = new UserModel(req.body);
     try {
         await newUser.save();
         res.status(201).send(newUser);
     } catch (error) {
-        // Server side error
-        res.status(500).send({message: `Error while inserting new user: ${error}`});
+        if (error.toString().includes("duplicate key error")) {
+            // Client side error -> username must be unique
+            return res.status(400).send({message: "Username or email is already in use."});
+        } else {
+            // Server side error
+            return res.status(500).send({message: `User not created: ${error}`});
+        }
     }
 })
 
